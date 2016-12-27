e3b2c8d1346628b1fa845fa93690dd5d5b0e5b6c
Zachary Barker <Zachary Barker>
2016-12-27 09:51:56 -0500

    Work flow that reads in xls files and creates cleaned data frames for each gauge

---
+++ RW Downstream Impacts/Code/WorkFlow.R
@@ -0,0 +1,38 @@
+################################################################################
+#
+# Work flow for RW downstream analysis
+#
+# Zac Barker, December 2016
+#
+################################################################################
+
+# Load necessary libraries
+library("gdata")                                                                # gdata must have perl library installed
+
+
+# Set working directory to main RW Downstream impacts folder
+setwd("C:/Users/Zachary/Desktop/SRG/RW Downstream Impacts")
+
+
+# Load user defined functions
+source("Code/CleanAndFormat.R")
+
+
+# Clean & format data
+setwd("Data/Raw Data/")
+Dresden <- CLEAN("Dresden_Flow.xls", "Dresden_Stage.xls", 12)
+Marseilles <- CLEAN("Marseilles_Flow.xls", "Marseilles_Stage.xls", 17)
+StarvedRock <- CLEAN("StarvedRock_Flow.xls", "StarvedRock_Stage.xls", 21)
+Peoria <- CLEAN("Peoria_Flow.xls", "Peoria_Stage.xls", 19)
+LaGrange <- CLEAN("LaGrange_Flow.xls", "LaGrange_Stage.xls", 21)
+setwd("../..")
+
+
+# Flow duration curves
+
+# Rating curve
+
+# T test
+
+
+
