c5366362c8d8e92b449aa948516ddf2f2f699774
Zachary Barker <Zachary Barker>
2017-02-17 17:15:45 -0500

    added pseudo code to cost function

--- RW Downstream Impacts/Code/RevenueReduction.R
+++ RW Downstream Impacts/Code/RevenueReduction.R
@@ -5,3 +5,20 @@
 # Zac Barker, February 2017
 #
 ################################################################################
+
+COST <- function(df, immersion, price) {
+     
+     # Creates a new column for revenue lost
+          
+          # For each time step (row):
+          
+          # Check if stage is lower than critical level
+          
+               # if not lower 
+                    # revenue lost = 0
+          
+               # if is lower:
+                    # (critical level - stage) * immersion factor * price
+  
+     return(df)
+}
